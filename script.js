9bedbd745ea15b752f69ed1b9a44de28dc29e8ec
CalebeLAR <ca6ele@gmail.com>
2022-07-13 15:35:11 -0400

    requisito 2 concluído

---
+++ script.js
@@ -0,0 +1,18 @@
+function alertLogin () {
+    const senha = document.querySelector("#password").value ;
+    const email = document.getElementById("email").value;
+    if (senha === "123456" && email === "tryber@teste.com") {
+        alert("Olá, Tryber!");
+    } else {
+        alert("Email ou senha inválidos.")
+    }
+
+}
+
+function click (){
+    document.getElementById("btn-login").addEventListener("click", alertLogin);
+
+}
+
+window.onload = click;
+
